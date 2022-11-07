380419aacb417576e344deaaa0a17a933fc6f0c4
Taiane Ferreira <72143305+taiferreira23@users.noreply.github.com>
2022-11-07 12:11:15 -0300

    Entrega modulo 06
    Ultima entrega recode pro

---
+++ ag_viagens/Models/Destinos.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Recode_Api_2022.Models
+{
+
+    [Table("Destinos")]
+    public class Destinos
+    {
+        [Key]
+        public int DestinoId { get; set; }
+        [Required(ErrorMessage = "Informe o nome do destino")]
+        [StringLength(50)]
+        public string Descricao { get; set; }
+        [Required(ErrorMessage = "Informe o Preço")]
+        public double Preço { get; set; }
+    }
+}
---
+++ ag_viagens/Models/DestinosDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Recode_Api_2022.Models
+{
+    public class DestinosDbContext: DbContext
+    {
+        public DestinosDbContext(DbContextOptions<DestinosDbContext> options)
+          : base(options)
+        { }
+        public DbSet<Destinos> Destinos { get; set; }
+
+    }
+}
+
+
